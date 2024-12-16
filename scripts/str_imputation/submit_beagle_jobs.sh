df1b0c1f12d4307421ba01de9284e61d028c1fc4
Yang_Li <yli091230@gmail.com>
2024-12-16 09:14:37 -0800

    add script for submit jobs on HPC

---
+++ scripts/str_imputation/submit_beagle_jobs.sh
@@ -0,0 +1,10 @@
+
+
+beagle_script="/expanse/protected/gymreklab-dbgap/mount/H3Africa/DS_I_Africa_project/from_ilifu/scripts/str_imputation/beagle.slurm"
+log_folder="/expanse/protected/gymreklab-dbgap/mount/H3Africa/DS_I_Africa_project/from_ilifu/logs/beagle_imputation"
+
+for chrom in $(seq 1 1); do
+  mkdir -p ${log_folder}/chr${chrom}
+  sbatch --job-name=chr${chrom} --output=${log_folder}/chr${chrom}/chr${chrom}_imputation_$(date +%Y%m%d)_%j.out ${beagle_script} ${chrom}
+
+done
